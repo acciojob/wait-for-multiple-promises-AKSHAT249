0acaf7c1dc03c87f23c56655484ec0da56706ca9
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-11-28 17:26:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
 //your JS code here. If required.
+function randomTime(){
+	return (Math.floor(Math.random() * 3)) + 1
+}
+
+const promise1 = () => {
+	let time = randomTime();
+	return (
+		new Promise(  (resolve, reject) => {
+			setTimeout( ( ) => {
+				resolve("Promise 1" + `${time}`)
+			}, time*1000 )
+		} )
+	)
+}
+
+
+const promise2 = () => {
+	let time = randomTime();
+	return (
+		new Promise(  (resolve, reject) => {
+			setTimeout( ( ) => {
+				resolve("Promise 2" + `${time}`)
+			}, time*1000 )
+		} )
+	)
+}
+
+const promise3 = () => {
+	let time = randomTime();
+	return (
+		new Promise(  (resolve, reject) => {
+			setTimeout( ( ) => {
+				resolve("Promise 3" + `${time}`)
+			}, time*1000 )
+		} )
+	)
+}
+
+let outputTable = document.getElementById("output")
+outputTable.innerHTML += `
+	<tr>
+		<td>Loading...</td>
+		<td>Loading...</td>
+	</tr>
+`
+
+
+Promise.all([promise1(), promise2(), promise3()]).then(
+	(data) =>{
+		outputTable.innerHTML = ''
+		data.map( (item)=> {
+			outputTable.innerHTML += `
+				<tr>
+					<td>${item.slice(0,item.length-1)}</td>
+					<td>${item.slice(-1)}</td>
+
+				</tr>
+			
+			`
+
+		} )
+		
+		
+
+		
+	}
+)
